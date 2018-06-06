4a6a5e26933af1f70331b82196c9523ef144a028
brunobieberstein <38557310+brunobieberstein@users.noreply.github.com>
2018-06-06 10:13:26 +0200

    Update geburtstags_aufgabe.py

--- combinatorics_and_monte_carlo/geburtstags_aufgabe.py
+++ combinatorics_and_monte_carlo/geburtstags_aufgabe.py
@@ -1,21 +1,29 @@
-# Geburtstagsparadoxon
-#
-# siehe auch:
-# https://de.wikipedia.org/wiki/Geburtstagsparadoxon
-
 import random
 
+def get_input():
+    while True:
+        eingabe = input('Wie viele Leute sind in dem Raum? ')
+        try:
+            n = int(eingabe)
+            return n
+            break
+        except ValueError:
+            print('Bitte Zahl eingeben.')
+
+def choices(seq, k):
+    result = []
+    for n in range(k):
+        result.append(random.choice(seq))
+    return result
 
 def calculate_likelihood_for_2_out_of_n(n):
     count_trials_with_people_with_same_birthday = 0
     trials = 10000
     for trial in range(trials):
-        birthdays = set(random.choices(range(365), k=n))
+        birthdays = set(choices(range(365), k=n))
         if len(birthdays) < n:
             count_trials_with_people_with_same_birthday += 1
     return count_trials_with_people_with_same_birthday / trials
 
-# TO DO:
-# Turn this into a program that asks the user for the number of people
-# in the room, then outputs the chances that two or more of them have the
-# same birthday!
+n = get_input()
+print('Die Wahrscheinlichkeit, dass mindestens zwei Leute am gleichen Tag Geburtstag haben beträgt:',calculate_likelihood_for_2_out_of_n(n))

7aa8615280c201b64503b2d6963cab13bd9fe6dd
brunobieberstein <38557310+brunobieberstein@users.noreply.github.com>
2018-06-06 11:46:16 +0200

    Update perseus_aufgabe.py

--- combinatorics_and_monte_carlo/perseus_aufgabe.py
+++ combinatorics_and_monte_carlo/perseus_aufgabe.py
@@ -1,26 +1,37 @@
-# Perseus, Pegasus und Andromeda: eine Variante des Ziegenproblems
-#
-# siehe Seite 7 in:
-# www.school-scout.de/vorschau/62770/mathematische-spiele-und-strategien-22012.pdf
-
 import random
 
-# set up the three caves
 caves = [1,2,3]
-# initialize counters for the two possible outcomes
 counter_andromeda_in_original_cave = 0
 counter_andromeda_in_third_cave = 0
 
+def kein_monster():
+    monster_liste = [1,2,3]
+    monster_liste.remove(random.choice(caves))
+    return monster_liste
+
+def wahl_1(caves):
+    wahl_1 = random.choice(caves)
+    return wahl_1
+
+def tipp(monster_liste, wahl_1):
+    if wahl_1 in monster_liste:
+        x = 1
+        tipp_liste = monster_liste[:]
+        tipp_liste.remove(wahl_1)
+        tipp = random.choice(tipp_liste)
+    else:
+        tipp = random.choice(monster_liste)
+        x = 0
+    return tipp, x
+
+
+
 trials = 10000
 for trial in range(trials):
-    # simulate and count outcomes
-    # TO DO!!
-    pass
+    counter_andromeda_in_third_cave += tipp(kein_monster(),wahl_1(caves))[1]
 
-# report results of simulation
 print(
     'Reconsidering his original choice would have been good for Perseus in '
-    '{0} out of {1} cases.'
-    .format(counter_andromeda_in_third_cave, trials)
+    '{0} out of {1} cases.'.format(counter_andromeda_in_third_cave, trials)
     )
     
